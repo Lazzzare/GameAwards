0ca3f0f5a9a1bbcbb6e88a0f8695d348a0e54139
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2022-05-03 16:48:37 +0400

    Awards

---
+++ script.js
@@ -0,0 +1,12 @@
+var anime = new Typed(".input", {
+    strings: [
+        "It Takes Two",
+        "The Last of Us: Part II",
+        "Sekiro: Shadows Die Twice",
+        "The Legend of Zelda: Breath of the Wild"
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
